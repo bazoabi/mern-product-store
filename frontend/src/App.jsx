e3a15069983cd89d79064cf7a958a0169a15ded5
Bader Zoabi <bader.zoabi@gmail.com>
2025-05-19 01:19:10 +0200

    Init Frontend via Vite, Install ChakraUI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Button colorScheme="teal" size="lg">
+        Button
+      </Button>
+    </>
+  );
+}
+
+export default App;
